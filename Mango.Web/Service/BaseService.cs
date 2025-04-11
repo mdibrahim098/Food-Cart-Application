0322d7ecfde565dd8f0a70c8d88fb13c822dcf0f
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-04-12 00:21:40 +0600

    Change 27

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,19 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory httpClientFactory) 
+        { 
+            _httpClientFactory = httpClientFactory;
+
+        }
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+           
+        }
+    }
+}
