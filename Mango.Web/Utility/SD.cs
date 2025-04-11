1cd1c6acdff771d6108c189c973dbf77348b6576
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-04-11 23:57:58 +0600

    Change 26

---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,16 @@
+﻿namespace Mango.Web.Utility
+{
+    public class SD
+    {
+
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+
+
+    }
+}
