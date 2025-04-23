f3a2f942a2f1c2e28a724159b30db9e4c23a01be
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-04-24 00:43:27 +0600

    Change 45 / Create all the model for authentication

---
+++ Mango.Services.AuthAPI/Models/Dto/LoginRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class LoginRequestDto
+    {
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
---
+++ Mango.Services.AuthAPI/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class LoginResponseDto
+    {
+        public string Token { get; set; }
+        public UserDto user { get; set; }
+
+    }
+}
