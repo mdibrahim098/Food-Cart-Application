ece434f48102587e4665a966c19444f17c9f93bd
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-04-24 01:27:31 +0600

    Change 48 / Complete Register Funtionality

--- Mango.Services.AuthAPI/Service/IService/IAuthService.cs
+++ Mango.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -5,7 +5,7 @@ namespace Mango.Services.AuthAPI.Service.IService
     public interface IAuthService
     {
         Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
-        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+         Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
 
     }
 }
