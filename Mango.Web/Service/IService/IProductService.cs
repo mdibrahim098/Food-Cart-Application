e2c930fc0e043cd4001eb3f7a7a994d27eaad0cc
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-04-27 03:07:16 +0600

    Change 74 Cosume Product APi from Web Project

---
+++ Mango.Web/Service/IService/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface IProductService
+    {
+
+
+        Task<ResponseDto?> GetProductAsync(string productCode);
+        Task<ResponseDto?> GetAllProductAsync();
+        Task<ResponseDto?> GetProductByIdAsync(int id);
+        Task<ResponseDto?> CreateProductsAsync(ProductDto productCode);
+        Task<ResponseDto?> UpdateProductAsync(ProductDto productCode);
+        Task<ResponseDto?> DeleteProductAsync(int id);
+    }
+}
