48f03f2ff2f09d467416fcfa9e10c3e304a42ee8
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-05-01 19:08:37 +0600

    Change 98

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -21,6 +21,47 @@ namespace Mango.Web.Controllers
         {
             return View( await LoadCartDtoBasedOnLoggedInUser());
         }
+        public async Task<IActionResult> Remove(int cartDetailsId)
+        {
+            var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            ResponseDto? response = await _cartService.RemoveFromCartAsync(cartDetailsId);
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Item removed from cart successfully";
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ApplyCoupon(CartDto cartDto)
+        {
+         
+            ResponseDto? response = await _cartService.ApplyCouponAsync(cartDto);
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Coupon Applied Successfully";
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveCoupon(CartDto cartDto)
+        {
+            cartDto.CartHeader.CouponCode = "";
+            ResponseDto? response = await _cartService.ApplyCouponAsync(cartDto);
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Coupon Remove Successfully";
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+
+        }
 
         private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
         {
