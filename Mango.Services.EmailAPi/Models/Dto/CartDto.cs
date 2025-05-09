34b302313de53d4f34c4b74b09c17afbc9199107
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-05-09 20:53:57 +0600

    Change 110 Added Email APi with Dtos

---
+++ Mango.Services.EmailAPi/Models/Dto/CartDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.EmailAPi.Models.Dto
+{
+    public class CartDto
+    {
+        public CartHeaderDto CartHeader { get; set; }
+        public IEnumerable<CartDetailsDto>? CartDetails { get; set; }
+
+    }
+}
