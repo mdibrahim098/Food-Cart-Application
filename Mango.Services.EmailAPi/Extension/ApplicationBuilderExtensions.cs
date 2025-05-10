81f35cb4672c1120ae875590a601b9f8d6f8105b
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-05-10 23:54:18 +0600

    Change 113 setup message start and stop receiving

---
+++ Mango.Services.EmailAPi/Extension/ApplicationBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection.Metadata;
+using Mango.Services.EmailAPi.Messaging;
+
+namespace Mango.Services.EmailAPi.Extension
+{
+    public static class ApplicationBuilderExtensions
+    {
+        private static IAzureServiceBusConsumer ServiceBusConsumer { get; set; }
+        public static IApplicationBuilder UseAzureServiceBusConsumer(this IApplicationBuilder app)
+        {
+            ServiceBusConsumer = app.ApplicationServices.GetService<IAzureServiceBusConsumer>();
+            var hostApplicationLife = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+
+            hostApplicationLife.ApplicationStarted.Register(OnStart);
+            hostApplicationLife.ApplicationStarted.Register(OnStop);
+
+            return app;
+        
+        }
+
+        private static void OnStop()
+        {
+            ServiceBusConsumer.Stop();
+        }
+
+        private static void OnStart()
+        {
+            ServiceBusConsumer.Start();
+        }
+    }
+}
--- Mango.Services.EmailAPi/Program.cs
+++ Mango.Services.EmailAPi/Program.cs
@@ -1,4 +1,6 @@
 using Mango.Services.EmailAPi.Data;
+using Mango.Services.EmailAPi.Extension;
+using Mango.Services.EmailAPi.Messaging;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddSingleton<IAzureServiceBusConsumer,AzureServiceBusConsumer>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -29,6 +31,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 ApplyMigration();
+app.UseAzureServiceBusConsumer();
 app.Run();
 
 
