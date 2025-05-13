99dfcff3f73c1ed086f1151f63d842c140aecc7a
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-05-14 00:06:16 +0600

    Change 146

---
+++ Mango.Services.RewardAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using Azure.Messaging.ServiceBus;
+using Mango.Services.RewardAPI.Message;
+using Mango.Services.RewardAPI.Services;
+using Newtonsoft.Json;
+
+namespace Mango.Services.RewardAPI.Messaging
+{
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
+    {
+        private readonly string serviceBusConnectionString;
+        private readonly string orderCreatedTopic;
+        private readonly string orderCreatedRewardsSubscription;
+        private readonly IConfiguration _configuration;
+        private readonly RewardService _rewardService;
+
+        private ServiceBusProcessor _rewardProcessor;
+        public AzureServiceBusConsumer(IConfiguration configuration, RewardService rewardService)
+        {
+            _configuration = configuration;
+            serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
+            orderCreatedTopic = _configuration.GetValue<string>("TopicAndQueueNames:OrderCreatedTopic");
+            orderCreatedRewardsSubscription = _configuration.GetValue<string>("TopicAndQueueNames:OrderCreated_Rewards_Subcription");
+            _rewardService = rewardService;
+            var client = new ServiceBusClient(serviceBusConnectionString);
+            _rewardProcessor = client.CreateProcessor(orderCreatedTopic, orderCreatedRewardsSubscription);
+           
+        }
+
+        public async Task Start()
+        {
+            _rewardProcessor.ProcessMessageAsync += OnNewOrderRewardsRequestReceived;
+            _rewardProcessor.ProcessErrorAsync += ErrorHandler;
+            await _rewardProcessor.StartProcessingAsync();
+
+        }
+        public async Task Stop()
+        {
+            await _rewardProcessor.StopProcessingAsync();
+            await _rewardProcessor.DisposeAsync();
+
+        }
+
+        private async Task OnNewOrderRewardsRequestReceived(ProcessMessageEventArgs args)
+        {
+            // this is where you will receive message
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            RewardsMessage ObjMessage = JsonConvert.DeserializeObject<RewardsMessage>(body);
+            try
+            {
+                //TODO - try to log email
+                await _rewardService.UpdateRewards(ObjMessage);
+                await args.CompleteMessageAsync(args.Message);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+ 
+
+            }
+
+        private Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+           Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+
+    }
+}
