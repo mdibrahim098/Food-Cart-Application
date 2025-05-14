ff241f48136838c2bddc60dbb55650f2f76648f7
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-05-15 01:30:11 +0600

    Change 154

---
+++ Mango.Web/wwwroot/Javascript/Order.js
@@ -0,0 +1,17 @@
+﻿var dataTable;
+function loadDataaTable() {
+    dataTable = $('#tblData').DataTable({
+
+        "ajax": { url: "order/getall" },
+        "columns": [
+            { data: "orderHeaderid", width: "5%" },
+            { data: "email", width: "25%" },
+            { data: "status", width: "10%" },
+            { data: "totalPrice", width: "10%" },
+            { data: "customerName", width: "10%" },
+
+        ],
+
+
+    });
+}
