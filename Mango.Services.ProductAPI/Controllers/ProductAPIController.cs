1d7c6e7e5973394fa541affc81a796317cf81eb8
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-05-16 16:39:32 +0600

    change 169

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -138,7 +138,17 @@ namespace Mango.Services.ProductAPI.Controllers
         {
             try
             {
-                Product obj = _db.Products.First(u => u.ProductId == id);
+                Product obj = _db.Products.First(u => u.ProductId == id);            
+                if(!string.IsNullOrEmpty(obj.ImageLocalPath))
+                {
+                    var oldFilePathDirectory = Path.Combine(Directory.GetCurrentDirectory(),obj.ImageLocalPath);
+                    FileInfo file = new FileInfo(oldFilePathDirectory);
+                     if(file.Exists)
+                    {
+                        file.Delete();
+                    }
+                }
+                
                 _db.Products.Remove(obj);
                 _db.SaveChanges();
 
