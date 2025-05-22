22d15254eea75a3541b662384c252d0673e90f5e
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-05-22 20:00:46 +0600

    Change 199

---
+++ Mango.Services.EmailAPi/Messaging/RabbitMQAuthConsumer.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Threading.Channels;
+using Mango.Services.EmailAPi.Serviecs;
+using RabbitMQ.Client;
+
+namespace Mango.Services.EmailAPi.Messaging
+{
+    public class RabbitMQAuthConsumer : BackgroundService
+    {
+
+        private readonly IConfiguration _configuration;
+        private readonly EmailService _emailService;
+        private IConnection _connection;
+        private IModel _channel;
+        public RabbitMQAuthConsumer(IConfiguration configuration,
+               EmailService emailService)
+        {
+
+            _configuration = configuration;
+            _emailService = emailService;
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(_configuration.GetValue<string>("TopicAndQueueNames:RegisterUserQueue"),
+                                  false, false, false, null);
+
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
