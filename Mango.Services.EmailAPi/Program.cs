9a4d00cfc9aa1c29274aacf1b14de1514ec4d61b
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-05-22 20:49:36 +0600

    Change 205

--- Mango.Services.EmailAPi/Program.cs
+++ Mango.Services.EmailAPi/Program.cs
@@ -16,7 +16,7 @@ var optionBuilder = new DbContextOptionsBuilder<AppDbContext>();
 optionBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 builder.Services.AddSingleton(new EmailService(optionBuilder.Options));
 
-
+builder.Services.AddHostedService<RabbitMQCartConsumer>();
 builder.Services.AddHostedService<RabbitMQAuthConsumer>();
 builder.Services.AddSingleton<IAzureServiceBusConsumer,AzureServiceBusConsumer>();
 builder.Services.AddControllers();
