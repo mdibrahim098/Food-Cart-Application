379e782ec97cf4b649b1e7d8310e3d39f7e62c82
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-05-22 18:50:13 +0600

    Change 196

---
+++ Mango.Services.AuthAPI/RabbitMQSender/RabbitMQAuthMessageSender.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace Mango.Services.AuthAPI.RabbitMQSender
+{
+    public class RabbitMQAuthMessageSender : IRabbitMQAuthMessageSender
+    {
+
+        private readonly string _hostName;
+        private readonly string _userName;
+        private readonly string _password;
+        private IConnection _connection;
+        public RabbitMQAuthMessageSender()
+        {
+            _hostName = "localhost";
+            _userName = "guest";
+            _password = "guest";
+        }
+        public void SendMessage(object message, string queueName)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _hostName,
+                UserName = _userName,
+                Password = _password
+            };
+            _connection = factory.CreateConnection();
+            using var channel = _connection.CreateModel();
+            channel.QueueDeclare(queue: queueName);
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+            channel.BasicPublish(exchange: "",routingKey: queueName,body: body);
+        }
+
+    }
+}
